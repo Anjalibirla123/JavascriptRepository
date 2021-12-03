efc10f9933bbe43d118e8e2a89e753caf69b205a
Anjalibirla123 <47386837+Anjalibirla123@users.noreply.github.com>
2021-12-03 14:13:10 +0530

    mm

--- src/behaviour/Alpha.js
+++ src/behaviour/Alpha.js
@@ -3,21 +3,7 @@ import Span from "../math/Span";
 import Behaviour from "./Behaviour";
 
 export default class Alpha extends Behaviour {
-  /**
-   * @memberof! Proton#
-   * @augments Proton.Behaviour
-   * @constructor
-   * @alias Proton.Alpha
-   *
-   * @todo add description for 'a' and 'b'
-   *
-   * @param {Number} a
-   * @param {String} b
-   * @param {Number} [life=Infinity] 				this behaviour's life
-   * @param {String} [easing=ease.easeLinear] 	this behaviour's easing
-   *
-   * @property {String} name The Behaviour name
-   */
+  
   constructor(a, b, life, easing) {
     super(life, easing);
 
@@ -25,20 +11,7 @@ export default class Alpha extends Behaviour {
     this.name = "Alpha";
   }
 
-  /**
-   * Reset this behaviour's parameters
-   *
-   * @method reset
-   * @memberof Proton#Proton.Alpha
-   * @instance
-   *
-   * @todo add description for 'a' and 'b'
-   *
-   * @param {Number} a
-   * @param {String} b
-   * @param {Number} [life=Infinity] 				this behaviour's life
-   * @param {String} [easing=ease.easeLinear] 	this behaviour's easing
-   */
+ 
   reset(a, b, life, easing) {
     this.same = b === null || b === undefined ? true : false;
     this.a = Span.setSpanValue(Util.initValue(a, 1));
@@ -47,15 +20,7 @@ export default class Alpha extends Behaviour {
     life && super.reset(life, easing);
   }
 
-  /**
-   * Sets the new alpha value of the particle
-   *
-   * @method initialize
-   * @memberof Proton#Proton.Alpha
-   * @instance
-   *
-   * @param {Proton.Particle} particle A single Proton generated particle
-   */
+  
   initialize(particle) {
     particle.data.alphaA = this.a.getValue();
 
@@ -63,15 +28,7 @@ export default class Alpha extends Behaviour {
     else particle.data.alphaB = this.b.getValue();
   }
 
-  /**
-   * @method applyBehaviour
-   * @memberof Proton#Proton.Alpha
-   * @instance
-   *
-   * @param {Proton.Particle} particle
-   * @param {Number} 			time the integrate time 1/ms
-   * @param {Int} 			index the particle index
-   */
+ 
   applyBehaviour(particle, time, index) {
     this.calculate(particle, time, index);
 
